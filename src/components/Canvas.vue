ad77318a4b1a3d683d19b935f641e3cac59faf69
eykamt <embergameslp@gmail.com>
2023-10-06 22:56:18 -0400

    add text and canvas size

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -11,7 +11,12 @@ const canvas = ref(null)
 const ctx = ref(null)
 onMounted(() => {
   ctx.value = canvas.value.getContext('2d')
+  canvas.value.width = 500
+  canvas.value.height = 500
   ctx.value.fillStyle = 'red'
-  ctx.value.fillRect(100, 100, 100, 100)
+  ctx.value.fillRect(0, 0, 100, 100)
+  ctx.value.fillStyle = "blue";
+  ctx.value.font = "bold 16px Arial";
+  ctx.value.fillText("Zibri", (100 / 2) - 17, (100 / 2) + 8);
 })
 </script>
