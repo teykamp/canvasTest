25386916c5ea50a80f5e7a3324b7cf05108eb017
eykamt <embergameslp@gmail.com>
2023-10-06 22:37:39 -0400

    working canvas

---
+++ src/components/Canvas.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <canvas ref="canvas"></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const canvas = ref(null)
+const ctx = ref(null)
+onMounted(() => {
+  ctx.value = canvas.value.getContext('2d')
+  ctx.value.fillStyle = 'red'
+  ctx.value.fillRect(100, 100, 100, 100)
+})
+</script>
