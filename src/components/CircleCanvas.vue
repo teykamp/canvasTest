0d0e8b59fccba37181e7dd5146bbb503f02b2951
Thomas Eykamp <embergameslp@gmail.com>
2024-05-23 00:14:59 -0400

    add selecting overlaps

--- src/components/CircleCanvas.vue
+++ src/components/CircleCanvas.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    <canvas ref="canvas" :width="width" :height="height" @mousedown="startDrag" @mousemove="drag" @mouseup="endDrag"
-      @mouseleave="endDrag" @dblclick="createCircle" @click.left="handleCanvasClick"
-      @click.right.prevent="deleteCircle"></canvas>
+    <canvas 
+      ref="canvas" 
+      :width="width" 
+      :height="height" 
+      @mousedown="startDrag" 
+      @mousemove="drag" 
+      @mouseup="endDrag"
+      @mouseleave="endDrag" 
+      @dblclick="createCircle" 
+      @click.left="handleCanvasClick"
+      @click.right.prevent="deleteCircle"
+    ></canvas>
   </div>
 </template>
 
@@ -33,14 +42,14 @@ const height = window.innerHeight - 30
 const currentCircleId = ref(0)
 const currentOverlapId = ref(0)
 const circlesSelectedByDrag = ref(false)
+const selectedOverlap = ref<Overlap | null>(null)
 
 const canvas = ref<HTMLCanvasElement | null>(null)
 const dragging = ref(false)
 const resizing = ref(false)
 const currentCircleIndex = ref<number | null>(null)
 const isDrawing = ref(false)
-const startPoint = reactive({ x: 0, y: 0 })
-const currentPoint = reactive({ x: 0, y: 0 })
+const selectionStartPoint = reactive({ x: 0, y: 0 })
 
 const circles = reactive<Circle[]>([])
 const overlaps = reactive<Overlap[]>([])
@@ -158,6 +167,7 @@ const endDrag = () => {
   resizing.value = false
   currentCircleIndex.value = null
   endSelection()
+  selectedOverlap.value = null
 }
 
 const createCircle = (event: MouseEvent) => {
@@ -178,29 +188,27 @@ const createCircle = (event: MouseEvent) => {
 
 const handleCanvasClick = (event: MouseEvent) => {
   const { x, y } = getMousePos(event)
-  // let foundOverlap = false
+  let foundOverlap = false
 
-  // const reversedOverlaps = overlaps.slice().reverse()
-  // // TODO: just run for loop in reverse
+  const reversedOverlaps = overlaps.slice().reverse()
+  // TODO: just run for loop in reverse
 
-  // for (let i = 0 i < reversedOverlaps.length i++) {
-  //   const overlap = reversedOverlaps[i]
-  //   const allInside = overlap.circles.every(circle => isInsideCircle(x, y, circle))
-  //   if (allInside && !foundOverlap) {
-  //     overlap.color = 'rgba(0, 255, 0, 0.5)'  // Turn overlap bright green
-  //     overlaps[i] = { ...overlap, color: 'rgba(0, 255, 0, 0.5)' }  // Ensure reactivity
-  //     console.log(overlap)
-  //     foundOverlap = true
-  //     break  // Exit early since we found an overlap
-  //   } else if (!allInside) {
-  //     overlaps[i] = { ...overlap, color: overlap.originalColor } // Restore original color if not selected
-  //   }
-  // }
+  for (let i = 0; i < reversedOverlaps.length; i++) {
+    const allInside = reversedOverlaps[i].circles.every(circle => isInsideCircle(x, y, circle))
+    if (allInside && !foundOverlap) {
+      circles.forEach(circle => circle.selected = false)
+      selectedOverlap.value = reversedOverlaps[i]
+      selectedOverlap.value.color = 'rgba(0, 255, 0, 1)'
+      foundOverlap = true
+      break
+    }
+  }
   
   const clickedCircleIndex = circles.findIndex(circle => isInsideCircle(x, y, circle))
 
   if (clickedCircleIndex === -1 && !circlesSelectedByDrag.value) {
     circles.forEach(circle => circle.selected = false)
+    selectedOverlap.value = null
   }
   drawCircles()
 
@@ -218,11 +226,11 @@ const isOverlapping = (circle1: Circle, circle2: Circle) => {
 const findOverlaps = (overlapGroup: Circle[], startIndex: number) => {
   if (overlapGroup.length > 1) {
     const overlapColors = [
-      'rgba(0, 0, 255, 0.5)',
-      'rgba(255, 0, 100, 0.5)',
-      'rgba(255, 0, 0, 0.5)',
-      'rgba(255, 255, 100, 0.5)',
-      'rgba(0, 255, 255, 0.5)'
+      'rgba(255, 190, 11, 1)',
+      'rgba(251, 86, 7, 1)',
+      'rgba(255, 0, 110, 1)',
+      'rgba(131, 56, 236, 1)',
+      'rgba(58, 134, 255, 1)'
     ]
     const color = overlapColors[overlapGroup.length - 2]
     overlaps.push({
@@ -258,6 +266,7 @@ const highlightOverlappingAreas = (ctx: CanvasRenderingContext2D) => {
   findOverlaps([], 0)
   // IMPORTANT for render order
   overlaps.sort((a, b) => a.circles.length - b.circles.length)
+  if (selectedOverlap.value) overlaps.push(selectedOverlap.value)
 
   overlaps.forEach(o => drawOverlappingAreas(ctx, o))
 }
@@ -286,7 +295,7 @@ const deleteCircle = () => {
 
 const startSelection = (event: MouseEvent) => {
   const { x, y } = getMousePos(event)
-  Object.assign(startPoint, { x, y })
+  Object.assign(selectionStartPoint, { x, y })
   isDrawing.value = true
 }
 
@@ -297,21 +306,21 @@ const drawSelection = (event: MouseEvent) => {
   const { x, y } = getMousePos(event)
   const currentPoint = { x, y }
 
-  const width = currentPoint.x - startPoint.x
-  const height = currentPoint.y - startPoint.y
+  const width = currentPoint.x - selectionStartPoint.x
+  const height = currentPoint.y - selectionStartPoint.y
 
   drawCircles()
 
   ctx.strokeStyle = 'blue'
   ctx.lineWidth = 1
-  ctx.strokeRect(startPoint.x, startPoint.y, width, height)
+  ctx.strokeRect(selectionStartPoint.x, selectionStartPoint.y, width, height)
   ctx.fillStyle = 'rgba(0, 0, 255, 0.2)'
-  ctx.fillRect(startPoint.x, startPoint.y, width, height)
+  ctx.fillRect(selectionStartPoint.x, selectionStartPoint.y, width, height)
 
-  const minX = Math.min(startPoint.x, currentPoint.x)
-  const maxX = Math.max(startPoint.x, currentPoint.x)
-  const minY = Math.min(startPoint.y, currentPoint.y)
-  const maxY = Math.max(startPoint.y, currentPoint.y)
+  const minX = Math.min(selectionStartPoint.x, currentPoint.x)
+  const maxX = Math.max(selectionStartPoint.x, currentPoint.x)
+  const minY = Math.min(selectionStartPoint.y, currentPoint.y)
+  const maxY = Math.max(selectionStartPoint.y, currentPoint.y)
 
   circles.forEach(circle => {
     circle.selected = (
@@ -323,6 +332,7 @@ const drawSelection = (event: MouseEvent) => {
 }
 
 const endSelection = () => {
+  selectedOverlap.value = null
   isDrawing.value = false
 }
 
